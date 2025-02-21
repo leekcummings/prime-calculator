23baf53eaafab773139fb0088c4b626822a67174
Lee Cummings <kierstenlcummings@gmail.com>
2025-02-21 11:12:51 -0500

    Add import and export to .csv options

---
+++ readWrite.py
@@ -0,0 +1,40 @@
+import os
+import re
+
+from Prime import Prime
+
+def importOptions():
+    """Import a .csv with a name of "primes_###.csv".
+    If multiple importable files exist, offer to import one by one."""
+    for f in os.listdir():
+        match = re.search(r"^primes_.+\.csv", f)
+        
+        if match != None:
+            choice = None
+            while choice not in ["y", "n"]:
+                choice = input(f"Do you want to import file \"{match.group(0)}\" (y/n)? ").lower()
+            if choice == "y":
+                return (match.group(0))
+    # If no files match regex, return None
+    return None
+
+def importCSV(path: str) -> list[Prime]:
+    primes = []
+    with open(path, "r") as f:
+        for line in f:
+            prime = line.strip().split(",")
+            primes.append(Prime(int(prime[0]), int(prime[1])))
+    f.close()
+    return primes
+
+def exportPrimes(primes: list[Prime], max: int):
+    """Export current list of Prime objects into an importable .csv format.
+    The file format is {prime number},{next composite number}"""
+    choice = None
+    while choice not in ["y", "n"]:
+        choice = input(f"Do you want to export objects as .csv (y/n)? ").lower()
+    if choice == "y":
+        with open(f"primes_{max}.csv", "w") as f:
+            for p in primes:
+                f.write(f"{p._prime},{p._comp}\n")
+        f.close()
